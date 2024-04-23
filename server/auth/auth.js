2f0f601fad03683a01f4b37b1a0c1f10f8cd055c
Harris <harrisgoodhand@DESKTOP-51ACIT7>
2024-04-23 16:13:05 -0400

    working on login post

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -5,12 +5,10 @@ const jwt = require("jsonwebtoken")
 const { PrismaClient } = require("@prisma/client")
 const express = require("express")
 const prisma = new PrismaClient()
-// const { JWT = JWT_SECRET } = process.env
 
 
 // POST register - create user with provided credentials and return a token
 authRouter.post("/register", async (req, res, next) => {
-  console.log("inside post")
   try {
     const newUser = await prisma.users.create({
       data: {
@@ -18,7 +16,7 @@ authRouter.post("/register", async (req, res, next) => {
         password: req.body.password
       },
     })
-    console.log("newUser: ", newUser)
+
     let token = null
     if(newUser){
       token = jwt.sign({ id: newUser.id }, process.env.JWT_SECRET)
@@ -31,14 +29,39 @@ authRouter.post("/register", async (req, res, next) => {
         message: "failed to create user"
       })
     }
-  } catch (error) {
+  }catch (error) {
     console.log(error)
     next(error)
   }
 })
 
 // POST login - log in with provided credentials and provide a token
-// authRouter.post("/login")
+authRouter.post("/login", async (req, res, next) => {
+  console.log("inside login post")
+  try {
+    const userLoggingIn = await prisma.users.findUnique({
+      where: {
+        username: req.body.username,
+        password: req.body.password
+      },
+    })
+    let token = null
+    if(userLoggingIn){
+      token = jwt.sign({ id: userLoggingIn.id }, process.env.JWT_SECRET)
+      res.send({
+        message: `Welcome back to juicebox, ${req.body.username}!`,
+        token: token
+      })
+    }else{
+      res.send({
+        message: "incorrect username or password"
+      })
+    }
+  }catch (error){
+    console.log(error)
+    next(error)
+  }
+})
 
 
 module.exports = authRouter
--- server/auth/authfunctions.js
+++ server/auth/authfunctions.js
@@ -1,4 +1,2 @@
+// will probably delete this page
 
-// POST register - create user with provided credentials and return a token
-
-// POST login - log in with provided credentials and provide a token
