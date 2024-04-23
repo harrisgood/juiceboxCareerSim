5318259332b1ae0a154eff19d14f2de4381b6b6b
Harris <harrisgoodhand@DESKTOP-51ACIT7>
2024-04-23 15:46:45 -0400

    more messing with auth route

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -1,21 +1,34 @@
+require('dotenv').config()
 const authRouter = require("express").Router()
 // const db = require("../db")
 const jwt = require("jsonwebtoken")
-const { PrismaClient, Prisma } = require("@prisma/client")
+const { PrismaClient } = require("@prisma/client")
+const express = require("express")
 const prisma = new PrismaClient()
+const { JWT = JWT_SECRET } = process.env
 
 authRouter.post("/register", async (req, res, next) => {
   console.log("inside post")
   try {
-    await prisma.post.create({
+    const newUser = await prisma.users.create({
       data: {
         username: req.body.username,
         password: req.body.password
       },
     })
-    res.send({
-      message: `Welcome to juicebox, ${req.body.username}!`
-    })
+    console.log("newUser: ", newUser)
+    let token = null
+    if(newUser){
+      token = jwt.sign({ id: newUser.id }, process.env.JWT)
+      res.send({
+        message: `Welcome to juicebox, ${req.body.username}!`,
+        token: token
+      })
+    }else{
+      res.send({
+        message: "failed to create user"
+      })
+    }
   } catch (error) {
     console.log(error)
     next(error)
@@ -23,17 +36,3 @@ authRouter.post("/register", async (req, res, next) => {
 })
 
 module.exports = authRouter
-
-
-// server.use((req, res, next) => {
-//   const auth = req.headers.authorization;
-//   const token = auth?.startsWith("Bearer ") ? auth.slice(7) : null;
-
-//   try {
-//     req.user = jwt.verify(token, process.env.JWT);
-//   } catch {
-//     req.user = null;
-//   }
-
-//   next();
-// });
